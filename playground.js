fcc5e9f3a36c013b71fd57a1deb07abebc2ebe0b
Sanu334 <maadarabuu132@gmail.com>
2024-06-13 02:09:45 +0530

    Recursive delete Code for BST added

--- playground.js
+++ playground.js
@@ -37,46 +37,71 @@ class BinarySearchTree {
     }
   }
 
-  delete(key) {
-    if (this.root === null) return;
-    let inLoop = true;
-    let presentNode = this.root;
-    let prevNode;
-    let direction;
-    while (inLoop) {
-      if (presentNode.key === key) {
-        if (presentNode.right === null && presentNode.left === null) {
-          if (direction) prevNode.right = null;
-          else prevNode.left = null;
-          inLoop = false;
-        } else if (presentNode.right !== null && presentNode.left === null) {
-          if (direction) prevNode.right = presentNode.right;
-          else prevNode.left = presentNode.right;
-          inLoop = false;
-        } else if (presentNode.right === null && presentNode.left !== null) {
-          if (direction) prevNode.right = presentNode.left;
-          else prevNode.left = presentNode.left;
-          inLoop = false;
-        } else {
-          let successor = this.inorderSuccesor(presentNode.right);
-          presentNode.key = successor.key;
-          presentNode.right = successor.right;
-          presentNode.left = successor.left;
-          console.log(presentNode);
-          inLoop = false;
-        }
+  // delete(key) {
+  //   if (this.root === null) return;
+  //   let inLoop = true;
+  //   let presentNode = this.root;
+  //   let prevNode;
+  //   let direction;
+  //   while (inLoop) {
+  //     if (presentNode.key === key) {
+  //       if (presentNode.right === null && presentNode.left === null) {
+  //         if (direction) prevNode.right = null;
+  //         else prevNode.left = null;
+  //         inLoop = false;
+  //       } else if (presentNode.right !== null && presentNode.left === null) {
+  //         if (direction) prevNode.right = presentNode.right;
+  //         else prevNode.left = presentNode.right;
+  //         inLoop = false;
+  //       } else if (presentNode.right === null && presentNode.left !== null) {
+  //         if (direction) prevNode.right = presentNode.left;
+  //         else prevNode.left = presentNode.left;
+  //         inLoop = false;
+  //       } else {
+  //         let successor = this.inorderSuccesor(presentNode.right);
+  //         presentNode.key = successor.key;
+  //         presentNode.right = successor.right;
+  //         presentNode.left = successor.left;
+  //         console.log(presentNode);
+  //         inLoop = false;
+  //       }
+  //     } else {
+  //       if (key > presentNode.key) {
+  //         prevNode = presentNode;
+  //         direction = true;
+  //         presentNode = presentNode.right;
+  //       } else {
+  //         prevNode = presentNode;
+  //         direction = false;
+  //         presentNode = presentNode.left;
+  //       }
+  //     }
+  //   }
+  // }
+
+  deleteRecursive(key) {
+    this.root = this.recursiveDelete(this.root, key);
+  }
+
+  recursiveDelete(node, key) {
+    if (key < node.key) {
+      node.left = this.recursiveDelete(node.left, key);
+    } else if (key > node.key) {
+      node.right = this.recursiveDelete(node.right, key);
+    } else {
+      if (node.left === null && node.right === null) {
+        return null;
+      } else if (node.right === null && node.left !== null) {
+        return node.left;
+      } else if (node.right !== null && node.left === null) {
+        return node.right;
       } else {
-        if (key > presentNode.key) {
-          prevNode = presentNode;
-          direction = true;
-          presentNode = presentNode.right;
-        } else {
-          prevNode = presentNode;
-          direction = false;
-          presentNode = presentNode.left;
-        }
+        let successor = this.inorderSuccesor(node.right);
+        node.key = successor.key;
+        node.right = this.recursiveDelete(node.right, successor.key);
       }
     }
+    return node;
   }
 
   inorderSuccesor(pN) {
